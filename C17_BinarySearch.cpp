25a091de2eb8ef6d556e0656cb42846eadfb2c67
LISTED MILE <listedmile@gmail.com>
2025-08-17 18:32:39 +0530

    binary search and bs by recursion done

--- C17_BinarySearch.cpp
+++ C17_BinarySearch.cpp
@@ -9,11 +9,11 @@ int find(vector<int> arr, int target)
     int mid;
     while (start <= end)
     {
-       // mid = (start + end) / 2;
-        
-       // if we add 2 intmax then mid will not be capable of holding it to cover it we can use ...
+        // mid = (start + end) / 2;
+
+        // if we add 2 intmax then mid will not be capable of holding it to cover it we can use ...
         mid = start + (end - start) / 2;
-        //same as above...mathemetically...
+        // same as above...mathemetically...
 
         if (target == arr[mid])
         {
@@ -31,19 +31,23 @@ int find(vector<int> arr, int target)
     return -1;
 }
 
-
-int findByRecursion(vector <int> arr, int target , int start , int end){
+int findByRecursion(vector<int> arr, int target, int start, int end)
+{
     int mid = start + (end - start) / 2;
-    if(start > end){
+    if (start > end)
+    {
         return -1;
     }
-    if( arr[mid] == target ){
+    if (arr[mid] == target)
+    {
         return mid;
     }
-    else if(arr[mid] > target){
+    else if (arr[mid] > target)
+    {
         return findByRecursion(arr, target, start, mid - 1);
     }
-    else if(arr[mid] < target){
+    else if (arr[mid] < target)
+    {
         return findByRecursion(arr, target, mid + 1, end);
     }
 }
@@ -65,10 +69,9 @@ int main()
         cout << target << " found at index : " << ansIndex << "\n";
     }
 
-
     // by recursion....
     cout << "\nBy recursion\n";
-    int ansIndexByRecursion = findByRecursion(arr, target , 0, arr.size()-1);
+    int ansIndexByRecursion = findByRecursion(arr, target, 0, arr.size() - 1);
     if (ansIndexByRecursion == -1)
     {
         cout << target << " not found \n";
