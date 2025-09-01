59aa3028798b230ef975efcd559cdd07b10d85c3
listedmile <listedmile@gmail.com>
2025-09-01 17:39:44 +0530

    insertion sort

---
+++ C23_3_SortingInsertionSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> InsertionSort(vector<int> arr)
+{
+    int len = arr.size();
+
+    for (int i = 1; i < len; i++)
+    {
+        int curr = arr[i];
+        int prevIndex = i - 1;
+        while (arr[prevIndex] > curr && prevIndex >= 0)
+        {
+            arr[prevIndex + 1] = arr[prevIndex];
+            prevIndex--;
+        }
+
+        arr[prevIndex + 1] = curr;
+    }
+    return arr;
+}
+
+int main()
+{
+
+    vector<int> arr = {2, 5, 4, 6, 3};
+    vector<int> ans = InsertionSort(arr);
+
+    cout << "Unsorted array : ";
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
+
+    cout << "\nSelection Sort : ";
+    for (int val : ans)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
