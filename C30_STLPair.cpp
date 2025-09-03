d286036b274fdf1157c2bee8deb0a5918dd0741a
listedmile <listedmile@gmail.com>
2025-09-03 19:44:29 +0530

    Pair

--- C30_STLPair.cpp
+++ C30_STLPair.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -9,4 +10,23 @@ int main(){
     // to access them ....
     cout << "P.first : " << p.first;
     cout << "\nP.second : " << p.second;
+
+    pair<int, pair<int, string>> pa = {1, {1, "a"}};
+    cout << "\npa.first : " << pa.first;
+    cout << "\npa.second.first : " << pa.second.first;
+    cout << "\npa.second.second : " << pa.second.second;
+
+    vector<pair<int, int>> arr = {{1, 2}, {1, 4}, {4, 5}, {3, 5}};
+    cout << "\nVector of Pairs : ";
+    for(auto val: arr){
+        cout << val.first << " " << val.second << "\t";
+    }
+
+    // dono same kam karte h bas fark h ki push back direct element ko push karta h aur emplace back pehle use object me convert karta h....
+    arr.push_back({4, 5});
+    arr.emplace_back(6, 7);
+    cout << "\nVector of pairs after push and emplace : ";
+    for(auto val: arr){
+        cout << val.first << " " << val.second << "\t";
+    }
 }
