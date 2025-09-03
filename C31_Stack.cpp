ff521461b217f49bc64a87429ead0db8b9d3278b
listedmile <listedmile@gmail.com>
2025-09-03 20:10:27 +0530

    Stack

---
+++ C31_Stack.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+
+    // LIFO
+    stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    cout << "The top element : " << s.top();
+
+    s.pop();
+    cout << "\nThe top element after deletion : " << s.top();
+
+    cout << "\nIs Stack s Empty : " << s.empty();
+
+    stack<int> s2;
+    s2.swap(s);
+     
+
+    return 0;
+}
