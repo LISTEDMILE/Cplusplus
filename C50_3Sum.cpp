43b8f4a82223090483616cfe8d2f87d05c1663cc
listedmile <listedmile@gmail.com>
2025-10-24 12:26:14 +0530

    3sum best approach

--- C50_3Sum.cpp
+++ C50_3Sum.cpp
@@ -33,7 +33,6 @@ set<vector<int>> find3Sum(vector<int> arr)
 
 set<vector<int>> find3SumOptimized(vector<int> arr)
 {
-
     set<vector<int>> ans;
     for (int i = 0; i < arr.size(); i++)
     {
@@ -49,6 +48,51 @@ set<vector<int>> find3SumOptimized(vector<int> arr)
                 ans.insert(trip);
             }
             s.insert(arr[j]);
+        }
+    }
+    return ans;
+}
+
+set<vector<int>> find3SumMostOptimized(vector<int> arr)
+{
+    set<vector<int>> ans;
+
+    // going to use 2 pointers approach so first sorting it..
+    sort(arr.begin(), arr.end());
+    int n = arr.size();
+    // take each idx value and then apply 2 pointers approach on rest to find matching sum here 0.
+    for (int i = 0; i < n; i++)
+    {
+        // without this also works but think of situation where there are multiple same values it will behave just like prev one so to ignore same triplate we skit this idx and find a new one.
+        if (i > 0 && arr[i] == arr[i - 1])
+        {
+            continue;
+        }
+        int j = i + 1, k = n - 1;
+        while (j < k)
+        {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0)
+            {
+                j++;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                ans.insert({arr[i], arr[j], arr[k]});
+                // yha bhi ek k-- se chlega but again imagine k-- kiya to 1st option wo bhi triplate bna le same j ke liye par 2 value j aur i wali whi h uske sath bhi triplate aur prev. k pe bhi matlab dono k same hi h . ya 2nd option nhi bna rha triplate to fir faeda hi kya ek iteration waste karne ka to sath sath j++ bhi kar diya.....
+                k--;
+                j++;
+
+                // same as i bina iske chlega but imagine repeating value waste of loop so jab tak same arr[j] milra same triplate milenge to skip that part..
+                while (j < k && arr[j] == arr[j - 1])
+                {
+                    j++;
+                }
+            }
         }
     }
     return ans;
@@ -70,6 +114,16 @@ int main()
 
     set<vector<int>> ansOptimized = find3SumOptimized(arr);
     for (vector<int> val : ansOptimized)
+    {
+        for (int item : val)
+        {
+            cout << item << "\t";
+        }
+        cout << endl;
+    }
+
+    set<vector<int>> ansMostOptimized = find3SumMostOptimized(arr);
+    for (vector<int> val : ansMostOptimized)
     {
         for (int item : val)
         {
