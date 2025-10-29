510d19b79afa7e4c979e52f4eaacfcdb89a5d768
listedmile <listedmile@gmail.com>
2025-10-29 21:17:50 +0530

    polymorphism

--- C73_Polymorphism.cpp
+++ C73_Polymorphism.cpp
@@ -20,25 +20,39 @@ public:
     }
 
     // function overloading...
-    void getInfo(){
+    void getInfo()
+    {
         cout << "Hi\n";
     }
 
-    void getInfo(int a){
+    void getInfo(int a)
+    {
         cout << "int : " << a << endl;
     }
 
-    void getInfo(char a){
+    void getInfo(char a)
+    {
         cout << "Char : " << a << endl;
+    }
+
+    virtual void hello()
+    {
+        cout << "Vertual Parent\n";
     }
 };
 
-class SectionA : public Student{
-    public:
-        int rollNo;
-        void getInfo(){
-            cout << "Function Overriding\n";
-        }
+class SectionA : public Student
+{
+public:
+    int rollNo;
+    void getInfo()
+    {
+        cout << "Function Overriding\n";
+    }
+    void hello()
+    {
+        cout << "Vertual Child\n";
+    }
 };
 
 int main()
@@ -46,10 +60,13 @@ int main()
 
     // when the same method or function behave differently..
 
-    // Types - 
+    // Types -
     // 1. Compile Time Polymorphism => Constructor Overloading, Function Overloading , Operator Overloading => when our function methods or constructors are multiple with same name but the no. of type of parameters should vary so which methods or constructor to execute will be decided by the type fo no. of arguments we give at creation of object or calling of function....
 
-    // 2. Run Time Polymorphism => function overriding => when parent and child have the same function with different implimentation.....parent class ka function overridden ho jata h.....
+    // 2. Run Time Polymorphism =>
+    // function overriding => when parent and child have the same function with different implimentation.....parent class ka function overridden ho jata h.....
+    // vertual function => similar to func overriding these are expected in parent class to be implemented by child class again...
+
     Student s1;
     Student s2("This");
 
@@ -59,5 +76,8 @@ int main()
 
     SectionA sec1;
     sec1.getInfo();
+
+    s2.hello();
+    sec1.hello();
     return 0;
 }
