ca21db35b455da07036bde9fdfb5ce3227beeffb
listedmile <listedmile@gmail.com>
2025-10-31 23:36:42 +0530

    linkedList

--- C76_LinkedList.cpp
+++ C76_LinkedList.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
-int main(){
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+    Node *head;
+    Node *tail;
+    public:
+    List(){
+        head = tail = NULL;
+    }
+};
+
+int main()
+{
+
     
     return 0;
 }
