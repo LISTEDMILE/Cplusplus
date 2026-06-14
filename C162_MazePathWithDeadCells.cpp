c3d801747693c15f586d72327d5ba15d7b752fc8
listedmile <listedmile@gmail.com>
2026-06-14 21:45:05 +0530

    maze path with daad cell

---
+++ C162_MazePathWithDeadCells.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+// we have to tell ki koi grid ya matrix ke kisi place pe hm kitne ways se pohoch skte h 00 se condition is ki sirf bottom ya right ja skte h
+// isko halka badalke hm i,j se 0,0 tak jaenge....
+
+int uniquePath(int row, int col, vector<vector<int>> &arr)
+{
+
+    if (row < 0 || col < 0)
+        return 0;
+
+    if (arr[row][col] == -1)
+        return 0;
+
+    if (row == 0 && col == 0)
+        return 1;
+
+    int up = uniquePath(row - 1, col, arr);
+    int left = uniquePath(row, col - 1, arr);
+
+    return up + left;
+}
+
+int uniquePathOptimalMemoization(int row, int col, vector<vector<int>> &p, vector<vector<int>> &arr)
+{
+    if (row < 0 || col < 0)
+        return 0;
+
+    if (arr[row][col] == -1)
+        return 0;
+
+    if (row == 0 && col == 0)
+        return 1;
+
+    if (p[row][col] != -1)
+        return p[row][col];
+    int up = uniquePathOptimalMemoization(row - 1, col, p, arr);
+    int left = uniquePathOptimalMemoization(row, col - 1, p, arr);
+
+    return p[row][col] = up + left;
+}
+
+// this time we will move from 00 and check ki pref me kitne ways the left me aur up me dono ko app karenge to curr ke ways aa jaenge...
+int uniquePathOptimalTabulation(int row, int col, vector<vector<int>> &p, vector<vector<int>> &arr)
+{
+    for (int i = 0; i <= row; i++)
+    {
+        for (int j = 0; j <= col; j++)
+        {
+            if (arr[i][j] == -1)
+                p[i][j] = 0;
+            else if (i == 0 && j == 0)
+            {
+                p[i][j] = 1;
+            }
+            else
+            {
+                int left = 0;
+                int up = 0;
+                if (i > 0)
+                {
+                    up = p[i - 1][j];
+                }
+                if (j > 0)
+                {
+                    left = p[i][j - 1];
+                }
+                p[i][j] = left + up;
+            }
+        }
+    }
+    return p[row][col];
+}
+
+int uniquePathMostOptimized(int row, int col, vector<vector<int>> &arr)
+{
+    vector<int> prev(col + 1, 0);
+    for (int i = 0; i <= row; i++)
+    {
+        vector<int> temp(col + 1, 0);
+        for (int j = 0; j <= col; j++)
+        {
+            if (arr[i][j] == -1)
+                temp[j] = 0;
+            else if (i == 0 && j == 0)
+            {
+                temp[0] = 1;
+            }
+            else
+            {
+                if (j == 0)
+                {
+                    temp[j] = prev[j];
+                }
+                else
+                {
+                    temp[j] = prev[j] + temp[j - 1];
+                }
+            }
+        }
+        prev = temp;
+    }
+    return prev[col];
+}
+
+int main()
+{
+
+    vector<vector<int>> arr = {{0, 0, 0}, {0, -1, 0}, {0, 0, 0}};
+    int i = arr.size();
+    int j = arr[0].size();
+
+    cout << "No. of unique paths : " << uniquePath(i - 1, j - 1, arr);
+
+    vector<vector<int>> p(i + 1, vector<int>(j + 1, -1));
+
+    cout << "\nNo. of unique paths (Optimal) : " << uniquePathOptimalMemoization(i - 1, j - 1, p, arr);
+
+    p = vector<vector<int>>(i + 1, vector<int>(j + 1, -1));
+
+    cout << "\nNo. of unique paths (Tabulation) : " << uniquePathOptimalTabulation(i - 1, j - 1, p, arr);
+
+    cout << "\nNo. of unique paths (Most Optimized) : " << uniquePathMostOptimized(i - 1, j - 1, arr);
+
+    return 0;
+}
